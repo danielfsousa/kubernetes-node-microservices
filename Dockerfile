ecb74bec3617dd16fc14141e0fece0db11211e1c
Daniel Sousa <sousa.dfs@gmail.com>
2019-01-28 00:18:02 -0200

    Add Continuous Delivery with Circleci (#2)

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM node:10.14-alpine
-ENV NODE_ENV production
 WORKDIR /usr/src/app
 COPY package*.json ./
-RUN npm install --production
+RUN npm install
 COPY . .
 EXPOSE 3000
 USER node
--- src/redis.js
+++ src/redis.js
@@ -1,18 +1,8 @@
 const redis = require('redis')
-const { RELEASE_NAME = '' } = process.env
+const { REDIS_HOST, REDIS_PORT } = process.env
 
-function getEnv (name) {
-  const releaseNameUpper = RELEASE_NAME.toUpperCase()
-  return releaseNameUpper
-    ? process.env[`${releaseNameUpper}_${name}`]
-    : process.env[name]
-}
-
-const REDIS_HOST = getEnv('REDIS_MASTER_SERVICE_HOST')
-const REDIS_PORT = getEnv('REDIS_MASTER_SERVICE_PORT')
-
-console.log('release name', RELEASE_NAME)
 console.log('redis host', REDIS_HOST)
+console.log('redis port', REDIS_PORT)
 
 module.exports = redis.createClient({
   host: REDIS_HOST,
